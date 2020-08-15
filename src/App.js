dadfb03730e30167336fe3b256ef8e0ce762d784
Joshua Lee <joshualee@joshuas-air-2.home>
2020-08-15 11:41:58 -1000

    adding states data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
+// https://disease.sh/v3/covid-19/states
 
 function App() {
-  return <div className="app">Hello World</div>;
+  const [states, setStates] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect has fired off");
+    const getStatesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/states").then((response) =>
+        response.json().then((data) => {
+          const states = data.map((state) => ({
+            name: state.state,
+          }));
+          setStates(states);
+        })
+      );
+    };
+    getStatesData();
+  }, []);
+
+  return (
+    <div className="app">
+      {/* Header */}
+      <div className="app_state_selector">
+        <FormControl className="app_dropdown">
+          <Select variant="standard" value="#">
+            {states.map((states) => (
+              <MenuItem value={states.value}>{states.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* State List */}
+      {/* Info Box */}
+      {/* Info Box */}
+      {/* Info Box */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
+    </div>
+  );
 }
 
 export default App;
